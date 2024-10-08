ba7f2000681a6c9c4a0326d08a8fe76ddfa44dee
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-08 09:19:12 -0700

    Add task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,9 +1,7 @@
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
-    return students.reduce(
-      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
-      0,
-    );
+    return students.reduce((sum, student) => sum + student.id, 0);
   }
+
   return 0;
 }
