7ff90b5b9d2bd0dc5116d37857e63ba6e186a547
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-08 11:09:07 -0700

    Add task 11

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,12 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const studentGrade = newGrades.filter(
-        (data) => data.studentId === student.id
+      const gradeObj = newGrades.find(
+        (grade) => grade.studentId === student.id
       );
       return {
         ...student,
-        grade: studentGrade.length > 0 ? studentGrade[0].grade : 'N/A',
+        grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
 }
