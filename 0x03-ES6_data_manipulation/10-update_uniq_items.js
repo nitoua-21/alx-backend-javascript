bbdca3a03b573b1aedf984be379e7f06b1e0b062
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-08 10:49:23 -0700

    Update task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,12 +2,9 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-
-  for (let [item, quantity] of map) {
-    if (quantity === 1) {
-      map.set(item, 100);
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
     }
-  }
-
-  return map;
+  });
 }
